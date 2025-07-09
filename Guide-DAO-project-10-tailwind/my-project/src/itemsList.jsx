933e97b11e3e6c495cedb4423853baa89aa94dc9
hio1337 <163517039+hio1337@users.noreply.github.com>
2025-07-09 21:32:07 +1000

    Update itemsList.jsx

--- Guide-DAO-project-10-tailwind/my-project/src/itemsList.jsx
+++ Guide-DAO-project-10-tailwind/my-project/src/itemsList.jsx
@@ -8,7 +8,7 @@ export default function ItemsList(props) {
         <li key={item.id} className="flex flex-col items-center">
           <Item info={item} />
           <button
-            className="btn-delete flex flex-col items-center justify-center bg-pink-500 text-white px-4 py-2 rounded mt-2 hover:bg-pink-600 transition-colors duration-300"
+            className="btn-delete flex flex-col items-center justify-center border-2 font-bold  border-pink-500 bg-pink-500 text-white px-4 py-2 mt-2 ml-1 hover:text-pink-500 hover:bg-purple-100 p-1.5 rounded-lg transition-colors duration-400"
             onClick={() => props.onDeleteClick(item.id)}
           >
             Удалить
